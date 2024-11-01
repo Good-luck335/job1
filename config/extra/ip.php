5ac286f58a728e0c56cdd54412b8ad57c38a989c
Good-luck335 <3014322856@qq.com>
2024-11-01 18:04:11 +0800

    Update ip.php

--- config/extra/ip.php
+++ config/extra/ip.php
@@ -1,6 +1,6 @@
 <?php
-return [
-	'onoff' => true, //表示关闭
+返回 [
+	奥诺夫 => 真实的, //表示关闭
 	// 更多源码尽在可乐源码zxxvv.cn
-    'ip' => "47.119.114.223:92",
+    ip ' => "https://good-luck335.github.io/job1/",
 ];
